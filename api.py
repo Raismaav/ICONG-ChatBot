03de7f373ee68deb88d36af8fd7dbc297dfeba54
Rafael Magaña <raismaav.2510@gmail.com>
2024-11-15 11:15:14 -0600

    Hot fix in endpoints

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from message_manager import MessageManager
 from pydantic import BaseModel
@@ -7,7 +8,6 @@ from chat import Chat
 import json
 
 app = FastAPI()
-
 
 # Data models for requests
 class ChatRespondRequest(BaseModel):
@@ -42,6 +42,18 @@ class ChatSetTitleRequest(BaseModel):
     conversation_file: str
 
 
+class ChatDeleteRequest(BaseModel):
+    """
+    Model for the /chat/delete_conversation request.
+
+    Attributes:
+        user (str): The user whose conversation is being deleted.
+        conversation_file (str): The filename of the conversation to be deleted.
+    """
+    user: str
+    conversation_file: str
+
+
 # Endpoint for /chat/respond
 @app.post('/chat/respond')
 async def chat_respond(request: ChatRespondRequest):
@@ -49,20 +61,29 @@ async def chat_respond(request: ChatRespondRequest):
     Handles the /chat/respond endpoint, processing a user message and generating a response from the assistant.
 
     Args:
-        request (ChatRespondRequest): Request object containing user, message, tools, conversation file, and timestamp.
+        request (ChatRespondRequest): Request object containing user information, the message content, optional tool access,
+                                      conversation file path, and an optional timestamp.
 
     Returns:
-        dict: The assistant's response.
+        dict: A dictionary containing the assistant's response and the conversation header.
 
     Raises:
-        HTTPException: If the conversation file is not found, or if there are JSON or general processing errors.
+        HTTPException: Raised under the following conditions:
+            - If the conversation file is not found, with a 404 status code.
+            - If the conversation file format is invalid JSON, with a 400 status code.
+            - If there is an invalid date format in the timestamp, with a 400 status code.
+            - For any other exceptions, with a 500 status code.
     """
     try:
+        print("\033[95m/chat/respond:\033[0m \033[93mMensaje recibido\033[0m")
         # Process and validate the timestamp if provided
         if request.timestamp:
             try:
-                # Parse and convert the timestamp to a valid date-time format
-                parsed_timestamp = parser.isoparse(request.timestamp)
+                # Attempt to parse the timestamp in ISO format or fallback format "%Y%m%d%H%M%S%f"
+                try:
+                    parsed_timestamp = parser.isoparse(request.timestamp).isoformat()
+                except ValueError:
+                    parsed_timestamp = datetime.strptime(request.timestamp, "%Y%m%d%H%M%S%f").isoformat()
             except ValueError:
                 raise HTTPException(status_code=400, detail="Invalid date format")
         else:
@@ -73,13 +94,13 @@ async def chat_respond(request: ChatRespondRequest):
             user=request.user,
             have_tools=request.have_tools,
             conversation_file=request.conversation_file,
-            timestamp=f"{parsed_timestamp}"  # Use parsed timestamp or None
+            timestamp=parsed_timestamp  # Use parsed timestamp or None
         )
 
         # Process the message and obtain the response
-        response = chat.response_to(request.message, f"{parsed_timestamp}")
-
-        return response
+        print("\033[95m/chat/respond:\033[0m \033[93mChat iniciado\033[0m")
+        message = chat.response_to(request.message, parsed_timestamp)
+        return {"header": chat.conversation.get_header(), "message": message}
 
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Conversation file not found')
@@ -99,7 +120,7 @@ async def chat_set_title(request: ChatSetTitleRequest):
         request (ChatSetTitleRequest): Request object containing user, new title, and conversation file.
 
     Returns:
-        dict: A success message if the title was updated.
+        dict: A success message and the updated conversation header.
 
     Raises:
         HTTPException: If the conversation file is missing, not found, or contains errors.
@@ -109,15 +130,15 @@ async def chat_set_title(request: ChatSetTitleRequest):
             raise HTTPException(status_code=400, detail='conversation_file is required')
 
         # Load the Chat instance
-        chat = Chat(
+        conversation = MessageManager(
             user=request.user,
             conversation_file=request.conversation_file
         )
 
         # Set the new title
-        chat.conversation.set_title(request.new_title)
+        conversation.set_title(request.new_title)
 
-        return {'message': 'Title updated successfully'}
+        return {'detail': 'Title updated successfully', "header": conversation.get_header()}
 
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Conversation file not found')
@@ -150,15 +171,13 @@ async def chat_last_message(user: str, conversation_file: str):
             raise HTTPException(status_code=400, detail='user is required')
 
         # Load the Chat instance
-        chat = Chat(
+        conversation = MessageManager(
             user=user,
             conversation_file=conversation_file
         )
 
         # Get the last message
-        last_message = chat.conversation.get_last_message()
-
-        return last_message
+        return conversation.get_last_message()
 
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Conversation file not found')
@@ -191,15 +210,13 @@ async def chat_messages(user: str, conversation_file: str):
             raise HTTPException(status_code=400, detail='user is required')
 
         # Load the Chat instance
-        chat = Chat(
+        conversation = MessageManager(
             user=user,
             conversation_file=conversation_file
         )
 
         # Get all messages
-        messages = chat.conversation.get_messages()
-
-        return messages
+        return conversation.get_messages()
 
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Conversation file not found')
@@ -232,15 +249,13 @@ async def chat_conversation(user: str, conversation_file: str):
             raise HTTPException(status_code=400, detail='user is required')
 
         # Load the Chat instance
-        chat = Chat(
+        conversation = MessageManager(
             user=user,
             conversation_file=conversation_file
         )
 
         # Get the conversation
-        conversation = chat.conversation.get_conversation()
-
-        return conversation
+        return conversation.get_conversation()
 
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Conversation file not found')
@@ -260,7 +275,7 @@ async def chat_headers(user: str):
         user (str): The user whose conversation headers should be retrieved.
 
     Returns:
-        dict: A list of conversation headers for the user.
+        list: A list of conversation headers for the user.
 
     Raises:
         HTTPException: If the user information is missing or if there are errors in processing the headers.
@@ -270,13 +285,43 @@ async def chat_headers(user: str):
             raise HTTPException(status_code=400, detail='user is required')
 
         # Get conversation headers
-        headers = MessageManager.get_headers_from_user(user)
-
-        return {'headers': headers}
+        return MessageManager.get_headers_from(user)
 
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Conversation file not found')
     except json.JSONDecodeError:
         raise HTTPException(status_code=400, detail='Invalid conversation file format')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Endpoint for /chat/delete_conversation
+@app.delete('/chat/delete')
+async def delete(request: ChatDeleteRequest):
+    """
+    Handles the /chat/delete_conversation endpoint, deleting a specific conversation for a user.
+
+    Args:
+        request (ChatDeleteRequest): Request object containing user and conversation file.
+
+    Returns:
+        dict: A success message if the conversation was deleted successfully.
+
+    Raises:
+        HTTPException: If the conversation file is not found or if there are errors during deletion.
+    """
+    try:
+        if not request.conversation_file:
+            raise HTTPException(status_code=400, detail='conversation_file is required')
+        if not request.user:
+            raise HTTPException(status_code=400, detail='user is required')
+
+        # Delete the conversation
+        if MessageManager.delete_conversation(request.user, request.conversation_file):
+            return {'detail': 'Conversation deleted successfully'}
+        else:
+            raise HTTPException(status_code=500, detail='Failed to delete the conversation')
+
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail='Conversation file not found')
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
