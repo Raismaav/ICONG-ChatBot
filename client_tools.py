6c0da326c5556b0a16a1aeb4b3c7750c5c7fcac9
Rafael Magaña <raismaav.2510@gmail.com>
2024-12-23 12:51:11 -0600

    rename client functions

--- client_tools.py
+++ client_tools.py
@@ -4,11 +4,14 @@ tools = [
         "type": "function",
         "function": {
             "name": "client_user",
-            "description": "Manda a llamar una funcion del cliente del servicio que retorna el nombre del cliente y el id si el parametro esta en true",
+            "description": "Manda a llamar una función del cliente del servicio que retorna el nombre del cliente y el ID si el parámetro está en true",
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "get_id": {"type": "boolean", "description": "trae el nombre y el id del cliente si esta en true"}
+                    "get_id": {
+                        "type": "boolean",
+                        "description": "Trae el nombre y el ID del cliente si está en true"
+                    }
                 },
                 "required": ["get_id"]
             }
@@ -17,75 +20,46 @@ tools = [
     {
         "type": "function",
         "function": {
-            "name": "client_address",
-            "description": "Manda a llamar una funcion del cliente que retorna la ip del cliente",
+            "name": "getIP",
+            "description": "Manda a llamar una función del cliente que retorna la IP del cliente",
             "parameters": None
         }
     },
-{
-      "type": "function",
-      "function": {
-        "name": "get_weather",
-        "description": "Determine weather in my location",
-        "parameters": {
-          "type": "object",
-          "properties": {
-            "location": {
-              "type": "string",
-              "description": "The city and state e.g. San Francisco, CA"
+    {
+        "type": "function",
+        "function": {
+            "name": "requestParameters",
+            "description": "Prueba de envío de parámetros, para después retornarlos de igual manera",
+            "parameters": {
+                "type": "object",
+                "required": [
+                    "string_param",
+                    "float_param",
+                    "int_param",
+                    "boolean_param"
+                ],
+                "properties": {
+                    "string_param": {
+                        "type": "string",
+                        "description": "Un parámetro de tipo string"
+                    },
+                    "float_param": {
+                        "type": "number",
+                        "description": "Un parámetro de tipo float"
+                    },
+                    "int_param": {
+                        "type": "integer",
+                        "description": "Un parámetro de tipo int"
+                    },
+                    "boolean_param": {
+                        "type": "boolean",
+                        "description": "Un parámetro de tipo booleano"
+                    }
+                },
+                "additionalProperties": False
             },
-            "unit": {
-              "type": "string",
-              "enum": [
-                "c",
-                "f"
-              ]
-            }
-          },
-          "additionalProperties": False,
-          "required": [
-            "location",
-            "unit"
-          ]
-        },
-        "strict": True
-      }
-    },
-{
-      "type": "function",
-      "function": {
-        "name": "request_parameters",
-        "description": "Preuba de envio de parametros, para despues retornarlos de igual manera",
-        "parameters": {
-          "type": "object",
-          "required": [
-            "string_param",
-            "float_param",
-            "int_param",
-            "boolean_param"
-          ],
-          "properties": {
-            "string_param": {
-              "type": "string",
-              "description": "Un parámetro de tipo string"
-            },
-            "float_param": {
-              "type": "number",
-              "description": "Un parámetro de tipo float"
-            },
-            "int_param": {
-              "type": "integer",
-              "description": "Un parámetro de tipo int"
-            },
-            "boolean_param": {
-              "type": "boolean",
-              "description": "Un parámetro de tipo booleano"
-            }
-          },
-          "additionalProperties": False
-        },
-        "strict": True
-      }
+            "strict": True
+        }
     }
 ]
 
